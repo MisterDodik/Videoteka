a1634171788fc9daf60ae4c2714fc6408c9e5550
“Drazen” <drazenpetrovic56@gmail.comcleargit config --global user.email drazenpetrovic56@gmail.com>
2024-05-30 02:05:31 +0200

    Izmjena signup.php interfacea i dodao signup.css

--- signup.php
+++ signup.php
@@ -1,24 +1,17 @@
 <html>
     <head>
-        <meta charset="utf-8">
+    <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Sign up</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-
-        <style>
-          body{
-            background-color: rgb(35, 54, 97);
-          } 
-          .inputclass {
-            font-size: 25px;
-          }
-          #colspanedit {
-            width: 100%;
-          }
-
-        </style>
+        <link rel="stylesheet" href="signup.css?v=<?php echo time(); ?>">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     </head>
 
@@ -82,62 +75,77 @@
 	?>
 
 	
-   <form action="signup.php" method="post">
-	<table width="500px" height="400px" border="0" cellpadding="0" cellspacing="0" bgcolor="white" style="background-color: rgb(14, 28, 59);">
-    <tr height="80px">
-        <td class="inputclass" style="color: white;">Sign Up</td>
-    </tr>
+<form action="signup.php" method="post">
+	  <div class="container">
+      <div class="title">
+        Create account
+      </div>
 
     <?php 
         if($msg_index==1) 
             echo "
-            <tr align='center'  height='50px'>
-                <td colspan='2'>
-                <p class='inputclass' style='color: red;'>There was an unknown error!</p>
-                </td>
-            </tr>";
+            <div>
+              <div class='inputclass'>There was an unknown error!<div>
+            </div>";
         elseif($msg_index==2) 
             echo "
-            <tr align='center'  height='50px'>
-                <td colspan='2'>
-                <p class='inputclass' style='color: red;'>Some of the information that you entered is not valid!</p>
-                </td>
-            </tr>";
+            <div>
+                <div class='inputclass'>Some of the information that you entered is not valid!</div>
+            </div>";
         elseif($msg_index==3) 
             echo "
-            <tr align='center'  height='50px'>
-                <td colspan='2'>
-                <p class='inputclass' style='color: red;'>User already exists!</p>
-                </td>
-            </tr>";
+            <div>
+              <div class='inputclass'>User already exists!</div>
+            </div>";
         elseif($msg_index==0) 
             echo "
-            <tr align='center'  height='60px'>
-                <td colspan='2'>
-                </td>
-            </tr>";
+            <div>
+                <div>
+                </div>
+            </div>";
     ?> 
+      <div class="input-container">
+        <div class="storage">
+          <label for="firstName">First name*</label>
+          <input id="firstName" class="inputclass" name="first_name" type="text" placeholder="Enter your first name"/>
+        </div>
+        <div class="storage">
+          <label for="lastName">Last name*</label>
+          <input id="lastName" class="inputclass" name="last_name" type="text" placeholder="Enter your last name"/>
+        </div>
+        <div class="storage">
+          <label for="emailInput">Email*</label>
+          <input id="emailInput" class="inputclass"  id="colspanedit" name="email" type="text" placeholder="Enter your email"/>
+        </div>
+        <div class="storage">
+          <label for="username">Username*</label>
+          <input id="username" class="inputclass" id="colspanedit" name="user" type="text" placeholder="Create your username"/>
+        </div>
+        <div class="storage">
+          <label for="passwordInput">Password*</label>
+          <input id="passwordInput" class="inputclass"  id="colspanedit" name="pass" type="password" placeholder="Create a password"/>
+        </div>
+        <div class="storage">
+          <input name="register" class="submit-button" type="submit" value="Create account" id="register"/>
+        </div>
+      </div>
+      <div class="log-option">
+          <div class="text">Already have an account? <a href="login.php">Log in</a></div>
+      </div>
 
-    <tr align="center" height="100px">
-		<td height="60px"><input class="inputclass" name="first_name" type="text" placeholder="First name"/></td>
-        <td height="60px"><input class="inputclass" name="last_name" type="text" placeholder="Last name"/></td>
-	  </tr>
-	  <tr align="center" height="100px">
-		<td height="60px" colspan="2"><input class="inputclass"  id="colspanedit" name="email" type="text" placeholder="Email"/></td>
-	  </tr>
-	  <tr align="center" height="100px">
-		<td height="60px" colspan="2"><input class="inputclass" id="colspanedit" name="user" type="text" placeholder="Username"/></td>
-	  </tr>
-	  <tr align="center" height="100px">
-		<td height="60px" colspan="2"><input class="inputclass"  id="colspanedit" name="pass" type="password" placeholder="New password"/></td>
-	  </tr>
-	  <tr align="center" height="100px">
-		<td colspan="2"><input name="register" class="inputclass" type="submit" value="Sign Up" id="register"/></td>
-	  </tr>
-	  <tr>
-		<td>&nbsp;</td>
-	  </tr>
-	</table>
+      
+
+    </div>
+
+    <div class="container2">
+      <img src="pics/pexels-cottonbro-4722571.jpg">
+      <div class="overlay-text">
+        <h1>Unlock the best of cinema</h1>
+        <div>
+        Welcome to our Top 100 Movies community! By signing up, you'll gain exclusive access to curated lists, in-depth reviews, and personalized recommendations that will elevate your movie-watching experience.
+        </div>
+      </div>
+    </div>
 	</form> 
    </td>
    
